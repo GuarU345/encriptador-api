f1d722b25834fcf88f7d0ea99a957f3a67eb4a05
Gael Emmanuel Acosta Serrano <21170116@uttcampus.edu.mx>
2024-09-30 21:12:41 -0600

    Token y cosas

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import {router} from './src/routes.js'
+import dotenv from 'dotenv'
 
 const app = express()
 const port = 3000
--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,14 +1,23 @@
 import { authService } from "../services/auth-service.js";
+import jwt from 'jsonwebtoken'
+
 
 const login = async (req, res) => {    
     const body = req.body
 
     try {
         const userLogin = await authService.login(body)
-        res.json({message:"Sesión Iniciada",user : userLogin})
+        
+        const token = jwt.sign({user : userLogin}, process.env.JWT_SECRET, {
+            expiresIn: '1h',
+        })
+        res.json({message:"Sesión Iniciada",user : userLogin.name, token})
+
     } catch (error) {
         res.json({error:error.message})
     }
+
+    return token
 }
 
 const register = async (req, res) => {
