670fddd50a621d8e7ed4875ed229c6c1bd852ebf
GuarU345 <abelmtz34@outlook.com>
2024-10-07 19:45:04 -0600

    añadir cors

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import {router} from './src/routes.js'
-import dotenv from 'dotenv'
-
+import cors from 'cors'
 const app = express()
 const port = 3000
 
+app.use(cors())
 app.use(express.json())
 app.use('/api',router)
 
